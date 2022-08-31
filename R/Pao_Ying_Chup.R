d19d38fa81412d96d8e495a7d4ec87cf62c71f92
AEKKAVIT LEIM <61969176+xnickky@users.noreply.github.com>
2022-08-31 14:18:50 +0700

    Update Pao_Ying_Chup.R

--- R/Pao_Ying_Chup.R
+++ R/Pao_Ying_Chup.R
@@ -8,7 +8,7 @@ win <- 0
 loss <- 0
 tie <- 0
  
-# Collect Vaule from users
+# Collect Value from users
 while(TRUE) {
     player <- as.numeric(readline("Time to choose !: Paper[1], Scissors[2], Rock[3], Quit[4]: "))
   if (player == 4) {
